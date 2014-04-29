fc7d847d92e8a46cf26a4cba75cb13342763e4ab
Alejandro Emanuel Paredes <alejandro.e.paredes@intel.com>
2014-04-29 12:13:39 -0400

    Improve Draft to add a new modal in the create instance.
    Clean form on topof the create instance
    modal.

--- openstack_dashboard/dashboards/project/instances/views.py
+++ openstack_dashboard/dashboards/project/instances/views.py
@@ -44,8 +44,6 @@ from openstack_dashboard.dashboards.project.instances \
     import tabs as project_tabs
 from openstack_dashboard.dashboards.project.instances \
     import workflows as project_workflows
-from openstack_dashboard.dashboards.admin.volumes \
-    import workflows as draft_project_workflows
 
 class IndexView(tables.DataTableView):
     table_class = project_tables.InstancesTable
@@ -123,27 +121,13 @@ class IndexView(tables.DataTableView):
                     exceptions.handle(self.request, msg)
         return instances
 
-class EditCapabilitiesAndRequirementsView(workflows.WorkflowView):
-    workflow_class = draft_project_workflows.EditCapabilitiesAndRequirements
-
-    def get_initial(self):
-        token = self.request.user.token.id
-        try:
-            # TODO(heather): needs to be a graffiti call
-            pass
-        except Exception:
-            url = reverse('horizon:admin:volumes:index')
-            exceptions.handle(self.request,
-                              _("Unable to retrieve volume tags."),
-                              redirect=url)
-        return {'token': token}
+class FilterView(forms.ModalFormView):
+    form_class = project_forms.FilterForm
+    template_name = 'project/instances/filter.html'
 
     def get_context_data(self, **kwargs):
-        context_data = super(EditCapabilitiesAndRequirementsView, self). \
-            get_context_data(**kwargs)
-        context_data['token'] = self.request.user.token.id
-        self.token = self.request.user.token.id
-        return context_data
+        context = super(FilterView, self).get_context_data(**kwargs)
+        return context
 
 class LaunchInstanceView(workflows.WorkflowView):
     workflow_class = project_workflows.LaunchInstance

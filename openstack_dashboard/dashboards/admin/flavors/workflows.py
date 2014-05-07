5bcd1316a4084e7112c5c491b9cd64255e118bba
Facundo Maldonado <facundo.n.maldonado@intel.com>
2014-05-07 10:10:08 -0300

    Capabilities widget added to falvors screen
    Capabilities widget added to flavors screen.
    Pending: Save updates extra specs

--- openstack_dashboard/dashboards/admin/flavors/workflows.py
+++ openstack_dashboard/dashboards/admin/flavors/workflows.py
@@ -19,6 +19,7 @@
 #    under the License.
 
 
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
 from horizon import exceptions
@@ -27,6 +28,7 @@ from horizon import workflows
 
 from openstack_dashboard import api
 
+INDEX_URL = "horizon:admin:flavors:index"
 
 class CreateFlavorInfoAction(workflows.Action):
     _flavor_id_regex = (r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-'
@@ -313,3 +315,89 @@ class UpdateFlavor(workflows.Workflow):
                                              'but unable to modify flavor '
                                              'access.'))
         return True
+
+
+class GraffitiCapabilitiesAction(workflows.EditGraffitiCapabilitiesAction):
+
+    class Meta:
+        name = _("Capabilities")
+        slug = 'capabilities'
+
+
+class GraffitiCapabilities(workflows.EditGraffitiCapabilitiesStep):
+    action_class = GraffitiCapabilitiesAction
+    depends_on = ('flavor_id',
+                  'flavor_type')
+
+    def __init__(self, workflow):
+        super(GraffitiCapabilities, self).__init__(workflow)
+
+        self.token = workflow.request.user.token.id
+        self.endpoint_id = None
+        for entry in workflow.request.user.service_catalog:
+            if entry['type'] == 'compute':
+                self.endpoint_id = entry['endpoints'][0]['id']
+                break
+        self.service_url = getattr(settings, 'GRAFFITI_URL', '')
+        # TODO(heather): take this out!
+        self.temp_token = getattr(settings, 'GRAFFITI_TOKEN', '')
+        self.temp_url = getattr(settings, 'GRAFFITI_TEMP_URL', '')
+        self.namespace_type_mapping = getattr(
+            settings, 'GRAFFITI_NAMESPACE_TYPE_MAPPING', '')
+
+    def prepare_action_context(self, request, context):
+        context = super(GraffitiCapabilities,
+                        self).prepare_action_context(request, context)
+        self.obj_id = context.get('flavor_id')
+        self.obj_type = context.get('flavor_type')
+        return context
+
+
+class GraffitiRequirementsAction(workflows.EditGraffitiRequirementsAction):
+
+    class Meta:
+        name = _("Requirements")
+        slug = 'requirements'
+
+
+class GraffitiRequirements(workflows.EditGraffitiRequirementsStep):
+    action_class = GraffitiRequirementsAction
+    depends_on = ('flavor_id',
+                  'flavor_type')
+
+    def __init__(self, workflow):
+        super(GraffitiRequirements, self).__init__(workflow)
+
+        self.token = workflow.request.user.token.id
+        self.endpoint_id = None
+        for entry in workflow.request.user.service_catalog:
+            if entry['type'] == 'compute':
+                self.endpoint_id = entry['endpoints'][0]['id']
+                break
+        self.service_url = getattr(settings, 'GRAFFITI_URL', '')
+        # TODO(heather): take this out!
+        self.temp_token = getattr(settings, 'GRAFFITI_TOKEN', '')
+        self.temp_url = getattr(settings, 'GRAFFITI_TEMP_URL', '')
+        self.namespace_type_mapping = getattr(
+            settings, 'GRAFFITI_NAMESPACE_TYPE_MAPPING', '')
+
+    def prepare_action_context(self, request, context):
+        context = super(GraffitiRequirements,
+                        self).prepare_action_context(request, context)
+        self.obj_id = context.get('flavor_id')
+        self.obj_type = context.get('flavor_type')
+        return context
+
+
+class EditCapabilitiesAndRequirements(workflows.Workflow):
+    slug = "edit_capabilities_and_requirements"
+    name = _("Edit Capabilities and Requirements")
+
+    success_message = _('Edited capabilities and requirements on "%s".')
+    failure_message = _('Unable to edit capabilities or requirements on "%s".')
+    success_url = "horizon:admin:flavors:index"
+    default_steps = (GraffitiCapabilities,
+                      GraffitiRequirements)
+
+    def handle(self, request, data):
+        return data
